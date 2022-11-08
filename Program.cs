4933332b03ef5fd680bdc4be42c64ed1690db8ca
hrishikesh kamble <hrishikamble13@gmail.com>
2022-11-08 22:38:44 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyser
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Problem");
+        }
+    }
+}
